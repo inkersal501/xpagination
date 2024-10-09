e1883ddaf198e55b7deff2aadf6a5a3ca1ef0f7d
inkersal501 <inkersal501@gmail.com>
2024-10-09 22:12:57 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Table from './Table';
+import { SnackbarProvider, useSnackbar } from "notistack";
 
 function App() {
   const API = 'https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json';
   const [apiData, setApiData] = useState([]);
-  
+  const { enqueueSnackbar } = useSnackbar();
+
   const getData = async ()=>{
     try {
       const data = await axios.get(API);
       return data.data;
     } catch (error) {
-      console.log(`Error Occured while fetching data ${error}`);
+      enqueueSnackbar(`failed to fetch data`,'error'); 
     }   
   };
 
@@ -23,16 +25,19 @@ function App() {
       setApiData(response);
     } 
     fetchData();
+    console.log(apiData)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   },[]);
 
   return (
+    <SnackbarProvider>
     <div className="App">
       <header className="App-header">
         <h1>Employee Data Table</h1>
       </header>
-      <Table data={apiData}/>
-      
+      {apiData.length>0 && <Table data={apiData}/>}       
     </div>
+    </SnackbarProvider>
   );
 }
 
